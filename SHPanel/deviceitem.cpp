c8130776c4f833d2c9539f57a051450bdd3b44fc
Truhov Anton <toha257@gmail.com>
2016-02-01 03:40:15 +0200

    Добавлено поле cid (id категории). Добавлен конструктор принимающий контейнер с устройством. Сменены название гетеров

--- SHPanel/deviceitem.cpp
+++ SHPanel/deviceitem.cpp
@@ -1,11 +1,20 @@
 #include "deviceitem.h"
 
-DeviceItem::DeviceItem(int id, QString name, QString type, QString state, QObject *parent) : QObject(parent)
+DeviceItem::DeviceItem(int id, QString name, QString type, QString state, int cid, QObject *parent) : QObject(parent)
 {
 	this->setId(id);
 	this->setName(name);
 	this->setType(type);
-	this->setState(state);
+    this->setState(state);
+    this->setCid(cid);
+}
+
+DeviceItem::DeviceItem(BoxDev dev, QObject *parent) : QObject(parent)
+{
+    this->setId(dev.id);
+    this->setName(dev.name);
+    this->setType(dev.type);
+    this->setState(dev.state);
 }
 
 int DeviceItem::id() const
@@ -13,27 +22,32 @@ int DeviceItem::id() const
 	return m_id;
 }
 
-QString DeviceItem::getName() const
+QString DeviceItem::name() const
 {
 	return m_name;
 }
 
-QString DeviceItem::getType() const
+QString DeviceItem::type() const
 {
 	return m_type;
 }
 
-QString DeviceItem::getState() const
+QString DeviceItem::state() const
 {
 	return m_state;
 }
 
+int DeviceItem::cid() const
+{
+    return m_cid;
+}
+
 void DeviceItem::setId(int id)
 {
-	if (m_id == id)
-		return;
+    if (m_id == id)
+        return;
 
-	m_id = id;
+    m_id = id;
 	emit idChanged(id);
 }
 
@@ -66,5 +80,14 @@ void DeviceItem::setState(QString state)
 
 void DeviceItem::changeState(QString state)
 {
-	emit sendState(state);
+	emit sendState(this->m_id, state);
+}
+
+void DeviceItem::setCid(int cid)
+{
+    if (m_cid == cid)
+        return;
+
+    m_cid = cid;
+    emit cidChanged(cid);
 }
--- SHPanel/deviceitem.h
+++ SHPanel/deviceitem.h
@@ -2,29 +2,35 @@
 #define DEVICEITEM_H
 
 #include <QObject>
+#include <QString>
+#include "containers.h"
 
 class DeviceItem : public QObject
 {
 	Q_OBJECT
 
 	Q_PROPERTY(int id READ id WRITE setId NOTIFY idChanged)
-	Q_PROPERTY(QString name READ getName WRITE setName NOTIFY nameChanged)
-	Q_PROPERTY(QString type READ getType WRITE setType NOTIFY typeChanged)
-	Q_PROPERTY(QString state READ getState WRITE setState NOTIFY stateChanged)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(QString type READ type WRITE setType NOTIFY typeChanged)
+    Q_PROPERTY(QString state READ state WRITE setState NOTIFY stateChanged)
+    Q_PROPERTY(int cid READ cid WRITE setCid NOTIFY cidChanged)
 
 public:
-	explicit DeviceItem(int id, QString name, QString type, QString state, QObject *parent = 0);
+    explicit DeviceItem(int id, QString name, QString type, QString state, int cid = 0, QObject *parent = 0);
+    DeviceItem(BoxDev dev, QObject *parent = 0);
 	int id() const;
-	QString getName() const;
-	QString getType() const;
-	QString getState() const;
+    QString name() const;
+    QString type() const;
+    QString state() const;
+    int cid() const;
 
 signals:
 	void idChanged(int id);
 	void nameChanged(QString name);
 	void typeChanged(QString type);
 	void stateChanged(QString state);
-	void sendState(QString state);
+	void sendState(int id, QString state);
+    void cidChanged(int cid);
 
 public slots:
 	void setId(int id);
@@ -32,12 +38,14 @@ public slots:
 	void setType(QString type);
 	void setState(QString state);
 	void changeState(QString state);
+    void setCid(int cid);
 
 private:
 	int m_id;
 	QString m_name;
 	QString m_type;
-	QString m_state;
+    QString m_state;
+    int m_cid;
 };
 
 #endif // DEVICEITEM_H
